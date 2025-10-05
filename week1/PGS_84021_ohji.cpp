b73bc850ec08f19f50420c781d1abfcfecef3d07
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-10-05 23:48:58 +0900

    [PGS 84021] 퍼즐 조각 채우기 - 지현 (bfs, simulation 개선 ver)

--- week1/PGS_84021_ohji.cpp
+++ week1/PGS_84021_ohji.cpp
@@ -1,49 +1,37 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <climits>
-#include <algorithm>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
 struct Point {
     int x, y;
-    bool operator<(const Point &oth) const {
-        if (x != oth.x) { return x < oth.x; }
+    bool operator<(const Point &oth) const { // 정렬 위해
+        if (x != oth.x) return x < oth.x;
         return y < oth.y;
     }
-    bool operator==(const Point &oth) const {
+    bool operator==(const Point &oth) const { // 같은지 비교 위해
         return x == oth.x && y == oth.y;
     }
 };
 
-vector<vector<int>> gboard, tboard;
-vector<vector<Point>> placeList;
-vector<vector<Point>> puzzleList[4];
-bool used[2502];
 int N;
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
+vector<vector<Point>> placeList, puzzleList[4];
 
-void normalize(vector<Point> &v) {
-    int minx = INT_MAX, miny = INT_MAX;
-    for (int i = 0; i < v.size(); ++i) { // minx, miny 찾고
-        minx = min(minx, v[i].x);
-        miny = min(miny, v[i].y);
-    }
-    
-    for (int i = 0; i < v.size(); ++i) { // 각 좌표를 좌상단이 0,0이 되도록 바꿔줌
-        v[i].x -= minx;
-        v[i].y -= miny;
-    }
-    
+void normalize(vector<Point> &v) { // 좌상단 0,0으로 & 좌표 순서 정렬
+    int minx = 1e9, miny = 1e9;
+    for (auto &p : v) { minx = min(p.x, minx); miny = min(p.y, miny); }
+    for (auto &p : v) { p.x -= minx; p.y -= miny; }
     sort(v.begin(), v.end());
 }
 
-vector<Point> collectBFS(int sx, int sy, vector<vector<int>> &board, int WALL) { // bfs로 공간들 모아서 넘기기
-    queue<Point> q; 
+vector<Point> shape(int sx, int sy, vector<vector<int>> &board, int WALL) {
     vector<Point> v;
+    queue<Point> q;
     q.push({sx, sy});
     v.push_back({sx, sy});
     board[sx][sy] = WALL;
@@ -53,23 +41,19 @@ vector<Point> collectBFS(int sx, int sy, vector<vector<int>> &board, int WALL) {
         
         for (int d = 0; d < 4; ++d) {
             int nx = cur.x + dx[d], ny = cur.y + dy[d];
-            
             if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
             if (board[nx][ny] == WALL) continue;
-                
-            board[nx][ny] = WALL;
+            
             q.push({nx, ny});
             v.push_back({nx, ny});
+            board[nx][ny] = WALL;
         }
     }
-    
-    // v를 정규화 + 정렬 시켜서 넘기기 => 이래야 모양 일치하는지 비교하기 편함
-    normalize(v);
-
+    normalize(v); // 좌상단 0,0으로 하고 좌표 순서를 정렬
     return v;
 }
 
-vector<Point> rotate90(const vector<Point> &prev) { // (x, y) -> (y, -x)
+vector<Point> rotate90(const vector<Point> &prev) {
     vector<Point> v;
     for (int i = 0; i < prev.size(); ++i) {
         v.push_back({prev[i].y, -prev[i].x});
@@ -78,53 +62,42 @@ vector<Point> rotate90(const vector<Point> &prev) { // (x, y) -> (y, -x)
     return v;
 }
 
-void init(const vector<vector<int>> &gb, const vector<vector<int>> &tb) {
-    gboard = gb;
-    tboard = tb;
-    N = gboard.size();
-}
-
-bool putPuzzle(int placeIdx, int puzzleIdx) {
+void pushPuzzle(vector<Point> v) {
     for (int r = 0; r < 4; ++r) {
-        if (placeList[placeIdx] == puzzleList[r][puzzleIdx]) {
-            used[puzzleIdx] = true;
-            return true;
-        }
+        puzzleList[r].push_back(v);
+        v = rotate90(v);
     }
-    return false;
 }
 
-int solution(vector<vector<int>> gb, vector<vector<int>> tb) {
-    init(gb, tb);
+int solution(vector<vector<int>> gboard, vector<vector<int>> tboard) {
+    N = gboard.size(); // game_board, table 행, 열 다 같음
     
-    // 빈 공간, 퍼즐 공간 리스트 생성
+    // 모양 모으기
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
             if (gboard[i][j] == 0) {
-                vector<Point> v = collectBFS(i, j, gboard, 1);
-                placeList.push_back(v);
+                placeList.push_back(shape(i, j, gboard, 1));
             }
-            
             if (tboard[i][j] == 1) {
-                vector<Point> v = collectBFS(i, j, tboard, 0);
-                puzzleList[0].push_back(v);
-                int idx = puzzleList[0].size() - 1;
-                // 퍼즐 회전
-                for (int r = 1; r < 4; ++r) {
-                    puzzleList[r].push_back(rotate90(puzzleList[r - 1][idx]));
-                }
+                pushPuzzle(shape(i, j, tboard, 0));
             }
         }
     }
 
     // 퍼즐 넣기
+    bool used[2502] = {}; // 퍼즐 썼는지 확인용
     int ans = 0;
-    for (int placeIdx = 0; placeIdx < placeList.size(); ++placeIdx) {
-        for (int puzzleIdx = 0; puzzleIdx < puzzleList[0].size(); ++puzzleIdx) {
-            if (used[puzzleIdx]) continue;
-            if (putPuzzle(placeIdx, puzzleIdx)) {
-                ans += placeList[placeIdx].size();
-                break;
+    for (auto &place : placeList) {
+        bool putted = false;
+        for (int r = 0; r < 4 && !putted; ++r) {
+            for (int i = 0; i < puzzleList[r].size(); ++i) {
+                if (used[i]) continue;
+                if (place == puzzleList[r][i]) {
+                    used[i] = true;
+                    putted = true;
+                    ans += place.size();
+                    break;
+                }
             }
         }
     }
