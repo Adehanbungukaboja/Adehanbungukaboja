51aa956e2dc2f743cf18cb96054ed5491b0490d4
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-10-05 22:27:46 +0900

    [PGS 84021] 퍼즐 조각 채우기 - 지현 (bfs, simulation... 챗지피티 도움 ver)

--- week1/PGS_84021_ohji.cpp
+++ week1/PGS_84021_ohji.cpp
@@ -1,140 +1,133 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <algorithm>
-#include <climits>
-#include <functional>
 #include <iostream>
-
-// 회전을 해서 꼭 들어맞는다면 무조건 넣기
-// 퍼즐 그룹핑을 하기
-// 4회전 하면서 넣을 수 있는 퍼즐이면 넣고 체크하기
-
-// 퍼즐 시작점, 그룹핑을 하고
-// 빈 공간 그룹핑을 해서
-// 회전시키면서 꼭 들어맞는 거 몇 개인지 체크하기
+#include <climits>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
 
 struct Point {
     int x, y;
+    bool operator<(const Point &oth) const {
+        if (x != oth.x) { return x < oth.x; }
+        return y < oth.y;
+    }
+    bool operator==(const Point &oth) const {
+        return x == oth.x && y == oth.y;
+    }
 };
 
-int board[52][52];
-int table[52][52];
-Point puzzles[2502]; // 퍼즐 리스트
-int whichPuzzle[2502]; // 빈 공간에 몇 번 퍼즐 넣었는지 체크용
-Point place[2502]; // 빈 공간 리스트
-int N, P, E;
+vector<vector<int>> gboard, tboard;
+vector<vector<Point>> placeList;
+vector<vector<Point>> puzzleList[4];
+bool used[2502];
+int N;
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 
-void puzzleBFS(int sx, int sy) {
-    ++P; // 퍼즐 번호 업데이트
-    puzzles[P].x = sx; // 퍼즐 찾기 쉽게 넣어놓기
-    puzzles[P].y = sy;
+void normalize(vector<Point> &v) {
+    int minx = INT_MAX, miny = INT_MAX;
+    for (int i = 0; i < v.size(); ++i) { // minx, miny 찾고
+        minx = min(minx, v[i].x);
+        miny = min(miny, v[i].y);
+    }
+    
+    for (int i = 0; i < v.size(); ++i) { // 각 좌표를 좌상단이 0,0이 되도록 바꿔줌
+        v[i].x -= minx;
+        v[i].y -= miny;
+    }
+    
+    sort(v.begin(), v.end());
+}
 
-    queue<Point> q;
+vector<Point> collectBFS(int sx, int sy, vector<vector<int>> &board, int WALL) { // bfs로 공간들 모아서 넘기기
+    queue<Point> q; 
+    vector<Point> v;
     q.push({sx, sy});
-    table[sx][sy] = P;
+    v.push_back({sx, sy});
+    board[sx][sy] = WALL;
     
     while (!q.empty()) {
         auto cur = q.front(); q.pop();
         
         for (int d = 0; d < 4; ++d) {
             int nx = cur.x + dx[d], ny = cur.y + dy[d];
+            
             if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-            if (table[nx][ny] != -1) continue;
-            
-            table[nx][ny] = P;
-            q.push({nx, ny});
-        }
-    }
-}
-
-void grouping() { // table에 놓인 퍼즐 그룹핑
-    int tmp[52][52] = {};
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            if (table[i][j] == -1) {
-                puzzleBFS(i, j);
-            }
-            
-        }
-    }
-
-    // for (int i = 0; i < N; ++i) {
-    //     for (int j = 0; j < N; ++j) {
-    //         cout << table[0][i][j] << ' ';
-    //     }
-    //     cout << '\n';
-    // }
-}
-
-void rotate() {
-    int newTable[52][52];
-
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            newTable[j][N - 1 - i] = table[i][j];
-        }
-    }
-    
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            table[i][j] = newTable[i][j];
-        }
-    }
-}
-
-void simulation() {
-    // 퍼즐 넣기
-    for (int r = 0; r < 4; ++r) {
-        for (int i = 0; i < N; ++i) {
-            for (int j = 0; j < N; ++j) {
+            if (board[nx][ny] == WALL) continue;
                 
-            }
+            board[nx][ny] = WALL;
+            q.push({nx, ny});
+            v.push_back({nx, ny});
         }
-        rotate();
-        
-        // for (int i = 0; i < N; ++i) {
-        //     for (int j = 0; j < N; ++j) cout << table[i][j] << ' ';
-        //     cout << '\n';
-        // }
-        // cout << '\n';
     }
-
     
+    // v를 정규화 + 정렬 시켜서 넘기기 => 이래야 모양 일치하는지 비교하기 편함
+    normalize(v);
+
+    return v;
 }
 
-void isOK(int bx, int by, int p) {
-    // 둘이 같이 bfs하면서 서로 같은 모양이면 ok
-    bool vis[]
-    queue<Point> q;
-    q.push({bx, by});
-    
-    while (!q.empty()) {
-        if (board[])
+vector<Point> rotate90(const vector<Point> &prev) { // (x, y) -> (y, -x)
+    vector<Point> v;
+    for (int i = 0; i < prev.size(); ++i) {
+        v.push_back({prev[i].y, -prev[i].x});
     }
+    normalize(v);
+    return v;
 }
 
-void init(vector<vector<int>> a, vector<vector<int>> b) {
-    N = a.size();
+void init(const vector<vector<int>> &gb, const vector<vector<int>> &tb) {
+    gboard = gb;
+    tboard = tb;
+    N = gboard.size();
+}
+
+bool putPuzzle(int placeIdx, int puzzleIdx) {
+    for (int r = 0; r < 4; ++r) {
+        if (placeList[placeIdx] == puzzleList[r][puzzleIdx]) {
+            used[puzzleIdx] = true;
+            return true;
+        }
+    }
+    return false;
+}
+
+int solution(vector<vector<int>> gb, vector<vector<int>> tb) {
+    init(gb, tb);
     
+    // 빈 공간, 퍼즐 공간 리스트 생성
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
-            board[i][j] = -a[i][j];
-            table[i][j] = -b[i][j]; // -1로 받아버리기
+            if (gboard[i][j] == 0) {
+                vector<Point> v = collectBFS(i, j, gboard, 1);
+                placeList.push_back(v);
+            }
+            
+            if (tboard[i][j] == 1) {
+                vector<Point> v = collectBFS(i, j, tboard, 0);
+                puzzleList[0].push_back(v);
+                int idx = puzzleList[0].size() - 1;
+                // 퍼즐 회전
+                for (int r = 1; r < 4; ++r) {
+                    puzzleList[r].push_back(rotate90(puzzleList[r - 1][idx]));
+                }
+            }
         }
     }
-}
 
-int solution(vector<vector<int>> a, vector<vector<int>> b) {
-    int answer = -1;
-    init(a, b);
+    // 퍼즐 넣기
+    int ans = 0;
+    for (int placeIdx = 0; placeIdx < placeList.size(); ++placeIdx) {
+        for (int puzzleIdx = 0; puzzleIdx < puzzleList[0].size(); ++puzzleIdx) {
+            if (used[puzzleIdx]) continue;
+            if (putPuzzle(placeIdx, puzzleIdx)) {
+                ans += placeList[placeIdx].size();
+                break;
+            }
+        }
+    }
 
-    grouping();
-    simulation();
-    
-    return answer;
+    return ans;
 }
